137b647c3cb480f49a5a823570d487d4fb7c0855
jasson99 <jasmine.baral@hotmail.com>
2019-12-31 12:12:42 +0545

    screenshots removed

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -73,11 +73,6 @@ module.exports = {
         start_process: START_PROCESS,
         server_path: chromedriver.path,
         cli_args: ['--port=' + SELENIUM_PORT]
-      },
-      screenshots: {
-        enabled: true,
-        path: 'tests/reports/screenshots',
-        on_failure: true
       }
     },
     drone: {
@@ -88,11 +83,6 @@ module.exports = {
           w3c: false
         },
         idleTimeout: 180
-      },
-      screenshots: {
-        enabled: true,
-        path: 'tests/reports/screenshots',
-        on_failure: true
       }
     },
     saucelabs: {
